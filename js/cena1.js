e74985eb90e0303a8d40d59f999104cc3e53151b
Gabjomar <gabjomar@gmail.com>
2022-05-27 17:00:43 +0000

    Corrigir Url WebSocket

--- js/cena1.js
+++ js/cena1.js
@@ -697,7 +697,7 @@ cena1.create = function () {
 
   //Conectar no servidor via WebSocket
   
-  this.socket = io("wss://stormy-beach-26933.herokuapp.com");
+  this.socket = io("https://secure-meadow-69283.herokuapp.com/");
 
   //Disparar evento quando jogador entrar na partida
   var self = this;
