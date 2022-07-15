7fdb2e10d948821982cace7c3756d281a2055875
Gabjomar <gabjomar@gmail.com>
2022-07-15 16:20:22 +0000

    Ajeitando texto contadorvencedor

--- js/cena1.js
+++ js/cena1.js
@@ -850,10 +850,10 @@ cena1.create = function () {
   somMouse = this.sound.add("somMouse");
   //Adicionando os contadores de partidas ganhas de cada player
   textoContadorVencedor0 = this.add
-    .text(635, 570, contadorVencedor0, fonteTexto2)
+    .text(625, 565, contadorVencedor0, fonteTexto2)
     .setVisible(false);
   textoContadorVencedor1 = this.add
-    .text(750, 570, contadorVencedor1, fonteTexto3)
+    .text(753, 565, contadorVencedor1, fonteTexto3)
     .setVisible(false);
   //Botão de ativar/desativar tela cheia
   botaoTelaCheia = this.add
